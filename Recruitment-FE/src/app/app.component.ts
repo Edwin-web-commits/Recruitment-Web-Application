012ca8bc9ca1bc4515b42cc4408f15ca93bd3a3f
Edwin Motlokwa <edwinmoropane@gmail.com>
2023-06-30 23:28:55 +0200

    Added basic structure for the App

--- Recruitment-FE/src/app/app.component.ts
+++ Recruitment-FE/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public links = [
+    {name: 'Candidates', href: '/', icon: 'table_rows'},
+    {name: 'Clients', href: '/clients', icon: 'view_list'},
+    {name:'Profile',href:'/profile',icon:'manage_accounts'},
+    {name:'Register',href:'/register',icon:'person_add'},
+    {name: 'Login', href: '/login', icon: 'login'},
+    {name: 'Logout', href: '/logout', icon: 'logout'},
+    
+    
+    ];
+  
   title = 'Recruitment-App';
 }
--- Recruitment-FE/src/app/app.module.ts
+++ Recruitment-FE/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule} from '@angular/material/list';
+import { MatIconModule} from '@angular/material/icon';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
